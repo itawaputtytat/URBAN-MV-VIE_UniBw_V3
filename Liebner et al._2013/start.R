838feedf75cb95a2a593ee95f4c34c4f63c7fa64
psych2datascience <psych2datascience@gmail.com>
2015-02-25 16:24:25 +0100

    due to externalization of plots

--- Liebner et al._2013/start.R
+++ Liebner et al._2013/start.R
@@ -1,5 +1,4 @@
 
-
 # Prepare data ------------------------------------------------------------
 
 ## Load data
@@ -31,36 +30,19 @@ plotdata <-
 plotdata
 
 
-# Simulation settings -----------------------------------------------------
+# Preparation of simulation data ------------------------------------------
 
 source("Liebner et al._2013/set4Liebner.R")
 
+## Add velocity trajectories to data
 data$k1 <- set4Liebner$k1_clust
 data$k2 <- set4Liebner$k2_clust
 data$k3 <- set4Liebner$k3_clust
 
-plotdata <-
-  plotdata +
-  geom_line(data = data,
-            aes(x = sxx_dist_m_rnd1,
-                y = k1),
-            col = "red",
-            size = 1) +
-  geom_line(data = data,
-            aes(x = sxx_dist_m_rnd1,
-                y = k2),
-            col = "green",
-            size = 1) +
-  geom_line(data = data,
-            aes(x = sxx_dist_m_rnd1,
-                y = k3),
-            col = "blue",
-            size = 1)
-plotdata
+## Explore data
+source("Liebner et al._2013/plot4exploration.R")
 
-
-# Preparation of simulation data ------------------------------------------
-
+## Preparation of simulation data
 source("Liebner et al._2013/prepare_data4sim.R")
 
 
@@ -75,42 +57,6 @@ source("Liebner et al._2013/compute_sim.R")
 
 # Visualize ---------------------------------------------------------------
 
-plotdata_sim <-
-  plotdata +
-
-  geom_vline(aes(xintercept = set4Liebner$poscarryout)) +
-  geom_rect(aes(xmin = set4Liebner$poscarryout - 6,
-                xmax = set4Liebner$poscarryout + 6,
-                ymin = 0, ymax = 1),
-            fill = "grey70") +
-  geom_text(aes(x = set4Liebner$poscarryout,
-                y = 0.5,
-                label = paste("simulation carried out at: \n",
-                              set4Liebner$poscarryout, " m", sep = ""),
-               fontface = "bold")) +
-
-  geom_rect(aes(xmin = min(datasim$sxx_dist_m_rnd1),
-                xmax = set4Liebner$poscarryout,
-                ymin = 1.1, ymax = 2.1),
-            fill = "grey80") +
-  geom_text(aes(x = (min(datasim$sxx_dist_m_rnd1) + (set4Liebner$poscarryout))/2,
-                y = 1.6,
-                label = paste("simulation starting:\n",
-                              set4Liebner$timelag, " s in past", sep = ""),
-                fontface = "bold")) +
-  geom_vline(aes(xintercept = min(datasim$sxx_dist_m_rnd1))) +
-
-  geom_vline(aes(xintercept = set4Liebner$objpos[set4Liebner$intention]),
-             colour = "red") +
-  geom_line(data = datasim,
-            aes(x = sxx_dist_m_rnd1,
-                y = speed_ms,
-                group = hypothesis,
-                colour = hypothesis)) +
-  scale_colour_manual(values = c(paste("red", c(1,3,4), sep = ""),
-                                 paste("green", c(1,3,4), sep = ""),
-                                 paste("blue", c(1,3,4), sep = "")))
-
-plot(plotdata_sim)
+source("Liebner et al._2013/plot4simulation.R")
 
 
